601dc388b971caeb61d094e9a22d5fd45612389f
DoniyorAli <doniyorfarhodov96@gmail.com>
2022-12-24 20:58:37 +0500

    car service is ready

--- services/car/car_service.go
+++ services/car/car_service.go
@@ -1,101 +1,113 @@
 package car
 
 import (
-	"MyProjects/RentCar_gRPC/rent_car_service/protogen/blogpost"
+	"MyProjects/RentCar_gRPC/rent_car_service/protogen/car"
 	"MyProjects/RentCar_gRPC/rent_car_service/storage"
 	"context"
-	"log"
 
 	"github.com/google/uuid"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-type authorService struct {
-	stg storage.StorageInter
-	blogpost.UnimplementedAuthorServiceServer
+type CarService struct {
+	Stg storage.StorageInter
+	car.UnimplementedCarServiceServer
 }
 
-func NewAuthorService(stg storage.StorageInter) *authorService {
-	return &authorService{
-		stg: stg,
-	}
-}
-
-func (s *authorService) Ping(ctx context.Context, req *blogpost.Empty) (*blogpost.Pong, error) {
-	log.Println("Ping")
-
-	return &blogpost.Pong{
-		Message: "OK",
-	}, nil
-}
-
-//?==============================================================================================================
-
-func (s *authorService) CreateAuthor(ctx context.Context, req *blogpost.CreateAuthorRequest) (*blogpost.CreateAuthorResponse, error) {
+// ?===============================================================================================================
+func (cs *CarService) CreateCar(ctx context.Context, req *car.CreateCarRequest) (*car.Car, error) {
 	id := uuid.New()
-
-	err := s.stg.AddAuthor(id.String(), req)
-
+	err := cs.Stg.AddCar(id.String(), req)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "s.stg.CreateAuthor: %s", err.Error())
+		return nil, status.Errorf(codes.Internal, "cs.Stg.CreateCar: %s", err.Error())
 	}
 
-	res, err := s.stg.GetAuthorById(id.String())
+	res, err := cs.Stg.GetCarById(id.String())
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "s.stg.GetArticleById: %s", err.Error())
+		return nil, status.Errorf(codes.Internal, "cs.Stg.GetBrandById: %s", err.Error())
 	}
 
-	return &blogpost.CreateAuthorResponse{
-		Id: res.Id,
+	return &car.Car{
+		CarId: res.CarId,
+		Model: res.Model,
+		Color: res.Color,
+		CarType: res.CarType,
+		Mileage: res.Mileage,
+		Year: res.Year,
+		Price: res.Price,
+		BrandId: res.BrandId, //! ??????????????????????? res.Brand.BramdId
+		CreatedAt: res.CreatedAt,
+		UpdatedAt: res.UpdatedAt,
 	}, nil
 }
 
-func (s *authorService) GetAuthorByID(ctx context.Context, req *blogpost.GetAuthorByIDRequest) (*blogpost.Author, error) {
-	author, err := s.stg.GetAuthorById(req.Id)
+// ?===============================================================================================================
+func (cs *CarService) GetCarByID(ctx context.Context, req *car.GetCarByIDRequest) (*car.GetCarByIDResponse, error) {
+	res, err := cs.Stg.GetCarById(req.Id)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "s.stg.GetAuthorById: %s", err.Error())
+		return nil, status.Errorf(codes.Internal, "cs.Stg.GetCarById: %s", err.Error())
 	}
-	return author, nil
-}
-
-func (s *authorService) GetAuthorList(ctx context.Context, req *blogpost.GetAuthorListRequest) (*blogpost.GetAuthorListResponse, error) {
-	res, err := s.stg.GetAuthorList(int(req.Offset), int(req.Limit), req.Search)
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, "s.stg.GetAuthorList: %s", err.Error())
-	}
-
 	return res, nil
 }
 
-func (s *authorService) UpdateAuthor(ctx context.Context, req *blogpost.UpdateAuthorRequest) (*blogpost.UpdateAuthorResponse, error) {
-
-	err := s.stg.UpdateAuthor(req)
-
+// ?===============================================================================================================
+func (cs *CarService) GetCarList(ctx context.Context, req *car.GetCarListRequest) (*car.GetCarListResponse, error) {
+	res, err := cs.Stg.GetCarList(int(req.Offset), int(req.Limit), req.Search)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "s.stg.UpdateAuthor: %s", err.Error())
+		return nil, status.Errorf(codes.Internal, "cs.Stg.GetCarList: %s", err.Error())
+	}
+	return res, nil
+}
+
+// ?===============================================================================================================
+func (cs *CarService) UpdateCar(ctx context.Context, req *car.UpdateCarRequest) (*car.Car, error) {
+	err := cs.Stg.UpdateCar(req.Id, req)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "cs.Stg.UpdateCar: %s", err.Error())
 	}
 
-	_, err = s.stg.GetAuthorById(req.Id)
-
+	res, err := cs.Stg.GetCarById(req.Id)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "s.stg.UpdateAuthor: %s", err.Error())
+		return nil, status.Errorf(codes.NotFound, "cs.Stg.UpdateCar: %s", err.Error())
 	}
 
-	return &blogpost.UpdateAuthorResponse{
-		Status: "Updated",
+	return &car.Car{
+		CarId: res.CarId,
+		Model: res.Model,
+		Color: res.Color,
+		CarType: res.CarType,
+		Mileage: res.Mileage,
+		Year: res.Year,
+		Price: res.Price,
+		BrandId: res.BrandId,
+		CreatedAt: res.CreatedAt,
+		UpdatedAt: res.UpdatedAt,
 	}, nil
 }
 
-func (s *authorService) DeleteAuthor(ctx context.Context, req *blogpost.DeleteAuthorRequest) (*blogpost.DeleteAuthorResponse, error) {
-
-	err := s.stg.DeleteAuthor(req.Id)
-
+// ?===============================================================================================================
+func (cs *CarService) DeleteCar(ctx context.Context, req *car.DeleteCarRequest) (*car.Car, error) {
+	res, err := cs.Stg.GetCarById(req.Id)
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, "s.stg.DeleteAuthor: %s", err.Error())
+		return nil, status.Errorf(codes.Internal, "cs.Stg.GetCarById: %s", err.Error())
 	}
 
-	return &blogpost.DeleteAuthorResponse{
-		Status: "Deleted",
+	err = cs.Stg.DeleteCar(req.Id)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "cs.Stg.DeleteCar: %s", err.Error())
+	}
+
+	return &car.Car{
+		CarId: res.CarId,
+		Model: res.Model,
+		Color: res.Color,
+		CarType: res.CarType,
+		Mileage: res.Mileage,
+		Year: res.Year,
+		Price: res.Price,
+		BrandId: res.BrandId,
+		CreatedAt: res.CreatedAt,
+		UpdatedAt: res.UpdatedAt,
 	}, nil
 }
